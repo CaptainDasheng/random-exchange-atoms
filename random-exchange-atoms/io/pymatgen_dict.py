31d96bb7db8838dbd515fb89922547ac4c2c2554
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-11-20 19:48:06 +0900

    Add pymatgen_dict.py to io folder

---
+++ random-exchange-atoms/io/pymatgen_dict.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+import pymatgen
+
+"""
+Initialize dict of pymatgen.Structure
+"""
+
+
+class PymatgenDict(object):
+    """
+    Initialize dict of pymatgen.Structure.
+    """
+    
+    @classmethod
+    def init_pymatgen_dict(cls, dict):
+        """
+        Initialize dict of pymetgen.Structure.
+        
+        In this classmethod, we set None to
+        a, b, c, alpha, beta, gamma and volume of pymatgen_dict["lattice"]
+        and xyz of all elements of pymatgen_dict["sites"].
+        
+        Arguments
+        ---------
+        dict: pymatgen.Structure.as_dict()
+            dict of pymatgen.Structure
+        
+        Returns
+        -------
+        dict: pymatgen.Structure.as_dict()
+            Edited dict of pymatgen.Structure
+        
+        !!!Cautions!!!
+            In this method we do not check given dict is generated from
+            pymatgen.Structure.as_dict().
+        """
+        for key in dict["lattice"].keys():
+            if key is "matrix":
+                pass
+            else
+                dict["lattice"][key] = None
+        
+        for specie in dict["species"]:
+            specie["xyz"] = None
+        
+        return dict
+
+if __name__ == "__main__":
+    pass
