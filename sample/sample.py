d52fb91547cebff52972129dbff9b0441cd72e10
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-11-21 16:20:57 +0900

    Add sample folder and output samples

---
+++ sample/sample.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+import logging
+import sys
+import os
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..'
+        )
+    )
+)
+import REA
+from REA.main import RandomExchangeAtoms as REA
+
+"""
+Sample for random-exchange-atoms.py
+"""
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    for i in range(3):
+        REA_object = REA(
+            "./sample_input/cif/Al2O3_hR30_R-3c_167.cif",
+            format="cif"
+        ).run_exchange(5)
+        REA_object.export_dict(filename="./sample_output/POSCAR"+str(i+1))
