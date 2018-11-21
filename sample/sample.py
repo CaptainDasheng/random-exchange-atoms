65a6991b3736059ecd29c0792998c1e567a8a6ba
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-11-21 16:37:18 +0900

    Modify samples

--- sample/sample.py
+++ sample/sample.py
@@ -23,9 +23,11 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    REA_object = REA(
+        "./sample_input/cif/Al2O3_hR30_R-3c_167.cif",
+        format="cif"
+    )
     for i in range(3):
-        REA_object = REA(
-            "./sample_input/cif/Al2O3_hR30_R-3c_167.cif",
-            format="cif"
-        ).run_exchange(5)
+        REA_object.init_struct_dict()
+        REA_object.run_exchange(5)
         REA_object.export_dict(filename="./sample_output/POSCAR"+str(i+1))
