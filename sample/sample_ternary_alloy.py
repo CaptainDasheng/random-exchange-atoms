610f5d01c4175bb69f3a86fa1a7b9719dacba786
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-12-20 15:20:34 +0900

    Add sample codes and output samples

--- sample/sample_ternary_alloy.py
+++ sample/sample_ternary_alloy.py
@@ -7,6 +7,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import pymatgen
 import REA
 from REA import RandomExchangeAtoms as Rea
 
@@ -18,5 +19,26 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    pass
+    structs_name_tuple = ("Fe4OF7", "Fe4OF8", "Fe6OF11",)
+    structs_dict = {}
+    for struct_name in structs_name_tuple:
+        structs_dict[struct_name] = pymatgen.Structure.from_str(
+            open("./sample_input/ternary_alloys/POSCAR_" + struct_name).read(),
+            fmt="poscar"
+        )
     
+    for struct_name, struct in structs_dict.items():
+        obj = Rea(
+            struct,
+            filename=struct_name,
+            atom_num_limit=50,
+            output_format="poscar",
+            output_path="./sample_output/sample_ternary_alloy/" + struct_name + "/"
+        )
+        for i in range(10):
+            obj.run_exchange(5)
+            obj.modify_cell_size()
+            obj.export_dict(
+                filename="./sample_output/sample_ternary_alloy/" + \
+                         struct_name + "/POSCAR"+str(i+1)
+            )
