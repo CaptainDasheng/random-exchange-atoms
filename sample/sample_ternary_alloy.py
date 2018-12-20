a3d321d5b81db3e9f1040f77a53a971dbbf28e61
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-12-20 13:20:02 +0900

    Create sample_ternary_alloy.py

---
+++ sample/sample_ternary_alloy.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright (c) 2018, Taku MURAKAMI. All rights reserved.
+# Distributed under the terms of the MIT License.
+
+import logging
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import REA
+from REA import RandomExchangeAtoms as Rea
+
+"""
+Sample for random-exchange-atoms.py to generate models of ternary_alloys.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    pass
+    
